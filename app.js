847519adf498dd9404339c178b16669eabbce49b
orutea <orutea02@gmail.com>
2026-05-10 14:42:23 +0900

    fix cards json path

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
    ==================================================== */
 
 // ★ここを自分のSupabaseの情報に書き換えてください
-const SUPABASE_URL = "https://ckbzdrngzpcjufzowvsl.supabase.co";
-const SUPABASE_KEY = "sb_publishable_FtT7aDlhc7HlRc_0KgE-ug_0i2bVrZr";
+const SUPABASE_URL = "https://rohpwisxpzpbnsqvyvzb.supabase.co";
+const SUPABASE_KEY = "sb_publishable_QmfPgzSkLR7oFIRu4vYcQQ_os4Gadqk";
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
